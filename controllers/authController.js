f0093db29df625e335c2bc0d24edafecf0fe0e9b
Uday-24 <chotaliyauday24@gmail.com>
2025-05-06 09:11:52 +0530

    Login added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
     let {username, email, password} = req.body;
@@ -37,6 +38,48 @@ const registerUser = async (req, res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+    let {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({ message: 'Email and password are required.' });
+    }
+
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message: 'Invalid email or password'});
+        }
+        
+        let isMatch = await bcrypt.compare(password, user.password);
+        
+        if(!isMatch){
+            return res.status(401).json({message: 'Invalid email or password'});
+        }
+
+        let access_token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+
+        res.status(200).json({
+            message: 'Login successful',
+            access_token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user,email
+            }
+        });
+
+    }catch(error){
+        console.error('Login error', error.message);
+        res.status(500).json({message: 'Server error'});
+    }
+}
+
 module.exports = {
     registerUser,
+    loginUser,
 }
